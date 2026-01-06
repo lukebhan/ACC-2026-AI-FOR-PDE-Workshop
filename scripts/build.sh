d5fac207982686ef6d8d5af8be0a3b613d7a218b
Luke <luke.bhan@vanderbilt.edu>
2026-01-06 12:26:37 -0800

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 
 ZOLA_VERSION=0.21.0
 
-curl -L https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz \
+curl -L https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-musl.tar.gz \
   | tar -xz
 
 ./zola build
